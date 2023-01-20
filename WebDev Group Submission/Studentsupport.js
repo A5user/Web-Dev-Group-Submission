755f3392dd36f1480e7b44b8c30d9be72b2eecf7
A5user <91368890+A5user@users.noreply.github.com>
2023-01-20 23:34:36 +0800

    Add files via upload

---
+++ WebDev Group Submission/Studentsupport.js
@@ -0,0 +1,95 @@
+// Get the JSON data
+fetch('faq-data.json')
+  .then(response => response.json())
+  .then(data => {
+    // Get the FAQ section element
+    let faqSection = document.getElementById("faq")
+
+    // Iterate over the data and create HTML elements
+    data.faq.forEach(item => {
+      let question = document.createElement("h3");
+      question.innerHTML = item.question;
+      question.classList.add("question-class")
+
+      let answer = document.createElement("p");
+      answer.innerHTML = item.answer;
+      answer.classList.add("answer-class")
+
+      faqSection.appendChild(question);
+      faqSection.appendChild(answer);
+    });
+
+    // Get the search input and suggestions container element
+let searchInput = document.getElementById("search-input");
+let suggestionsContainer = document.getElementById("suggestions");
+let searchButton = document.getElementById("search-button");
+
+// Add an event listener to the search button
+searchButton.addEventListener("click", function() {
+    // Get the search query
+    let searchQuery = searchInput.value.toLowerCase();
+
+// Fetch the JSON data
+fetch('faq-data.json')
+.then(response => response.json())
+.then(data => {
+  // Get the FAQ section element
+  let faqSection = document.querySelector(".faq");
+  faqSection.innerHTML = ''; // clear the current content in FAQ section
+
+  // Filter the data by search query
+  let filteredData = data.faq.filter(item => {
+    return item.question.toLowerCase().indexOf(searchQuery) !== -1 || item.answer.toLowerCase().indexOf(searchQuery) !== -1;
+  });
+
+  // Iterate over the filtered data and create HTML elements
+  filteredData.forEach(item => {
+    let question = document.createElement("h3");
+    question.innerHTML = item.question;
+
+    let answer = document.createElement("p");
+    answer.innerHTML = item.answer;
+
+    faqSection.appendChild(question);
+    faqSection.appendChild(answer);
+});
+});
+});
+searchInput.addEventListener("keyup", function() {
+// code here is same as above
+});
+
+// Add an event listener to the search input
+searchInput.addEventListener("input", function() {
+  // Get the search query
+  let searchQuery = searchInput.value.toLowerCase();
+
+  // Fetch the JSON data
+  fetch('faq-data.json')
+    .then(response => response.json())
+    .then(data => {
+      // Filter the data by search query
+      let filteredData = data.faq.filter(item => {
+        return item.question.toLowerCase().indexOf(searchQuery) !== -1 || item.answer.toLowerCase().indexOf(searchQuery) !== -1;
+      });
+
+      // Get the first 3 suggestions
+      let suggestions = filteredData.slice(0, 3);
+
+      // Clear the current suggestions
+      suggestionsContainer.innerHTML = "";
+
+      // Iterate over the suggestions and create HTML elements
+      suggestions.forEach(item => {
+        let suggestion = document.createElement("p");
+        suggestion.innerHTML = item.question;
+        suggestionsContainer.appendChild(suggestion);
+      });
+    });
+});
+
+
+
+
+
+  });
---
+++ WebDev Group Submission/feedback.js
@@ -0,0 +1,51 @@
+function submitForm() {
+    // Get the form elements
+    const courseSelect = document.getElementById("course-select");
+    const rating = document.getElementById("rating");
+    const feedback = document.getElementById("feedback");
+    const recommendation = document.querySelector('input[name="recommendation"]:checked');
+    const recommendationComments = document.getElementById("recommendation-comments");
+
+
+    if (!courseSelect.value) {
+      alert("Please select a course.");
+      return;
+    }
+
+    if (!rating.value) {
+      alert("Please provide a rating for the course.");
+      return;
+    }
+
+    if (!feedback.value) {
+      alert("Please provide feedback for the course.");
+      return;
+    }
+
+    if (!recommendation) {
+      alert("Please select whether you would recommend the course to others.");
+      return;
+    }
+
+    // Submit
+    alert(`Form submitted successfully!
+    Course: ${courseSelect.value}
+    Rating: ${rating.value}
+    Feedback: ${feedback.value}
+    Recommendation: ${recommendation.value}
+    Recommendation Comments: ${recommendationComments.value}`);
+
+    // Reset
+    courseSelect.value = "";
+    rating.value = "";
+    feedback.value = "";
+    recommendation.checked = false;
+    recommendationComments.value = "";
+  }
+  document.getElementById("feedback-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for your feedback!");
+    //Resetting the form
+    document.getElementById("feedback-form").reset();
+  });
+  

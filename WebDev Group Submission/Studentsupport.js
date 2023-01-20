ddcc488167a980a9d9bd23a9503d05a733018278
Muhammad Hashim <91368890+A5user@users.noreply.github.com>
2023-01-20 23:49:20 +0800

    Update Studentsupport.js

--- WebDev Group Submission/Studentsupport.js
+++ WebDev Group Submission/Studentsupport.js
@@ -2,10 +2,10 @@
 fetch('faq-data.json')
   .then(response => response.json())
   .then(data => {
-    // Get the FAQ section element
+    // FAQ 
     let faqSection = document.getElementById("faq")
 
-    // Iterate over the data and create HTML elements
+   
     data.faq.forEach(item => {
       let question = document.createElement("h3");
       question.innerHTML = item.question;
@@ -19,30 +19,29 @@ fetch('faq-data.json')
       faqSection.appendChild(answer);
     });
 
-    // Get the search input and suggestions container element
+    //Search input and suggestions
 let searchInput = document.getElementById("search-input");
 let suggestionsContainer = document.getElementById("suggestions");
 let searchButton = document.getElementById("search-button");
 
-// Add an event listener to the search button
 searchButton.addEventListener("click", function() {
-    // Get the search query
+ 
     let searchQuery = searchInput.value.toLowerCase();
 
-// Fetch the JSON data
+
 fetch('faq-data.json')
 .then(response => response.json())
 .then(data => {
-  // Get the FAQ section element
+ 
   let faqSection = document.querySelector(".faq");
-  faqSection.innerHTML = ''; // clear the current content in FAQ section
+  faqSection.innerHTML = ''; // clear 
 
-  // Filter the data by search query
+  // Filter 
   let filteredData = data.faq.filter(item => {
     return item.question.toLowerCase().indexOf(searchQuery) !== -1 || item.answer.toLowerCase().indexOf(searchQuery) !== -1;
   });
 
-  // Iterate over the filtered data and create HTML elements
+ // filtered data and write
   filteredData.forEach(item => {
     let question = document.createElement("h3");
     question.innerHTML = item.question;
@@ -56,30 +55,30 @@ fetch('faq-data.json')
 });
 });
 searchInput.addEventListener("keyup", function() {
-// code here is same as above
+
 });
 
-// Add an event listener to the search input
+
 searchInput.addEventListener("input", function() {
-  // Get the search query
+ 
   let searchQuery = searchInput.value.toLowerCase();
 
-  // Fetch the JSON data
+ 
   fetch('faq-data.json')
     .then(response => response.json())
     .then(data => {
-      // Filter the data by search query
+      
       let filteredData = data.faq.filter(item => {
         return item.question.toLowerCase().indexOf(searchQuery) !== -1 || item.answer.toLowerCase().indexOf(searchQuery) !== -1;
       });
 
-      // Get the first 3 suggestions
+     
       let suggestions = filteredData.slice(0, 3);
 
-      // Clear the current suggestions
+      
       suggestionsContainer.innerHTML = "";
 
-      // Iterate over the suggestions and create HTML elements
+      
       suggestions.forEach(item => {
         let suggestion = document.createElement("p");
         suggestion.innerHTML = item.question;
